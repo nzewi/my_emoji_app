791623d50b791899599e4191461aff15377cb43b
nzewi <nelsonnzewi2001@gmail.com>
2022-04-28 10:19:32 +0100

    Added emojis
    Added 3 laughing emojis

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const myEmojis = ["👨‍💻", "⛷", "🍲", "😂", "🤣"];
+const myEmojis = ["👨‍💻", "⛷", "🍲", "😂", "🤣", "🤣"];
 
 function renderEmojis() {
   const emojiContainer = document.getElementById("emoji-container");
